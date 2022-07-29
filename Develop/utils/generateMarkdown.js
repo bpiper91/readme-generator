e1ee1b80b4ee07586bd72ea7a163f95dbf659155
bpiper91 <bpiper91@gmail.com>
2022-07-29 00:28:32 -0400

    updated generateMarkdown with all but license data functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,24 +1,81 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return license;
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return license;
+};
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// generate the license section of README
+function renderLicenseSection(license) {
+  if (license) {
+  // if there is a license, add the section to the page
+    return `
 
-// TODO: Create a function to generate markdown for README
+## License
+    
+${renderLicenseLink(license)}`
+  } else {
+    // if there is no license, return an empty string
+    return "";
+  };
+};
+
+// if there is a license, add it to the table of contents
+function renderLicenseTOC(license) {
+  if (license) {
+    return `
+- [License](#license)`;
+  } else {
+    return "";
+  };
+};
+
+// generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+${renderLicenseBadge(data.license)}
 
-    ## Description
+## Description
 
-    ${data.description}
+${data.description}
 
+## Table of Contents
 
+- [Installation](#installation)
+- [Usage](#usage)${renderLicenseTOC(data.license)}
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contribution}
+
+## Tests
+
+${data.test}
+
+## Questions
+
+GitHub: [${data.github}](https://github.com/${data.github})
+
+Email address: ${data.email}
+
+${data.reach}
 `;
 }
 
