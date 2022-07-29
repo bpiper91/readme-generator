a8ccd4d6489ea4c3955bcbae7d7a416b488eaae0
bpiper91 <bpiper91@gmail.com>
2022-07-29 13:50:19 -0400

    updated license list and added matching array in generateMarkdown.js, updated license functions to reference array and return values

--- Develop/index.js
+++ Develop/index.js
@@ -64,43 +64,134 @@ const questions = [
     },
     {   // license
         type: 'list',
-        name: 'license',
+        name: 'licenseIndex',
         message: 'Please select the appropriate license.',
         choices: [
-            {name: 'Academic Free License v3.0', value: 'afl-3.0'},
-            {name: 'Apache license 2.0', value: 'apache-2.0'},
-            {name: 'Artistic license 2.0', value: 'artistic-2.0'},
-            {name: 'Boost Software License 1.0', value: 'bsl-1.0'},
-            {name: 'BSD 2-clause "Simplified" license', value: 'bsd-2-clause'},
-            {name: 'BSD 3-clause "New" or "Revised" license', value: 'bsd-3-clause'},
-            {name: 'BSD 3-clause Clear license', value: 'bsd-3-clause-clear'},
-            {name: 'Creative Commons license family', value: 'cc'},
-            {name: 'Creative Commons Zero v1.0 Universal', value: 'cc0-1.0'},
-            {name: 'Creative Commons Attribution 4.0', value: 'cc-by-4.0'},
-            {name: 'Creative Commons Attribution Share Alike 4.0', value: 'cc-by-sa-4.0'},
-            {name: 'Do What The F*ck You Want To Public License', value: 'wtfpl'},
-            {name: 'Educational Community License v2.0', value: 'ecl-2.0'},
-            {name: 'Eclipse Public License 1.0', value: 'epl-1.0'},
-            {name: 'Eclipse Public License 2.0', value: 'epl-2.0'},
-            {name: 'European Union Public License 1.1', value: 'eupl-1.1'},
-            {name: 'GNU Affero General Public License v3.0', value: 'agpl-3.0'},
-            {name: 'GNU General Public License family', value: 'gpl'},
-            {name: 'GNU General Public License v2.0', value: 'gpl-2.0'},
-            {name: 'GNU General Public License v3.0', value: 'gpl-3.0'},
-            {name: 'GNU Lesser General Public License family', value: 'lgpl'},
-            {name: 'GNU Lesser General Public License v2.1', value: 'lgpl-2.1'},
-            {name: 'GNU Lesser General Public License v3.0', value: 'lgpl-3.0'},
-            {name: 'ISC', value: 'isc'},
-            {name: 'LaTeX Project Public License v1.3c', value: 'lppl-1.3c'},
-            {name: 'Microsoft Public License', value: 'ms-pl'},
-            {name: 'MIT', value: 'mit'},
-            {name: 'Mozilla Public License 2.0', value: 'mpl-2.0'},
-            {name: 'Open Software License 3.0', value: 'osl-3.0'},
-            {name: 'PostgreSQL License', value: 'postgresql'},
-            {name: 'SIL Open Font License 1.1', value: 'ofl-1.1'},
-            {name: 'University of Illinois/NCSA Open Source License', value: 'ncsa'},
-            {name: 'The Unlicense', value: 'unlicense'},
-            {name: 'zLib License', value: 'zlib'}
+            // any change to this array needs to be reflected in the one used by generateMarkdown.js
+            {
+                name: 'Apache License 2.0',
+                value: 0
+              },
+              {
+                name: 'Boost Software License 1.0',
+                value: 1
+              },
+              {
+                name: 'BSD 3-Clause License',
+                value: 2
+              },
+              {
+                name: 'BSD 2-Clause License',
+                value: 3
+              },
+              {
+                name: 'Creative Commons CC0 1.0',
+                value: 4
+              },
+              {
+                name: 'Creative Commons Attribution 4.0 International',
+                value: 5
+              },
+              {
+                name: 'Creative Commons Attribution-ShareAlike 4.0 International',
+                value: 6
+              },
+              {
+                name: 'Creative Commons Attribution-NonCommercial 4.0 International',
+                value: 7
+              },
+              {
+                name: 'Creative Commons Attribution-NoDerivates 4.0 International',
+                value: 8
+              },
+              {
+                name: 'Creative Commons Attribution-NonCommmercial-ShareAlike 4.0 International',
+                value: 9
+              },
+              {
+                name: 'Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International',
+                value: 10
+              },
+              {
+                name: 'Eclipse Public License 1.0',
+                value: 11
+              },
+              {
+                name: 'GNU GPL v3',
+                value: 12
+              },
+              {
+                name: 'GNU GPL v2',
+                value: 13
+              },
+              {
+                name: 'GNU AGPL v3',
+                value: 14
+              },
+              {
+                name: 'GNU LGPL v3',
+                value: 15
+              },
+              {
+                name: 'GNU FDL v1.3',
+                value: 16
+              },
+              {
+                name: 'The Hippocratic License 2.1',
+                value: 17
+              },
+              {
+                name: 'The Hippocratic License 3.0',
+                value: 18
+              },
+              {
+                name: 'IBM Public License Version 1.0',
+                value: 19
+              },
+              {
+                name: 'Open Source Initiative ISC License',
+                value: 20
+              },
+              {
+                name: 'The MIT License',
+                value: 21
+              },
+              {
+                name: 'Mozilla Public License 2.0',
+                value: 22
+              },
+              {
+                name: 'Open Data Commons Attribution License (BY)',
+                value: 23
+              },
+              {
+                name: 'Open Database License (ODbL)',
+                value: 24
+              },
+              {
+                name: 'Public Domain Dedication and License (PDDL)',
+                value: 25
+              },
+              {
+                name: 'Perl Artistic License 2.0',
+                value: 26
+              },
+              {
+                name: 'SIL Open Font License 1.1',
+                value: 27
+              },
+              {
+                name: 'The Unlicense',
+                value: 28
+              },
+              {
+                name: 'The Do What the F*ck You Want to Public License',
+                value: 29
+              },
+              {
+                name: 'The zlib/libpng License',
+                value: 30
+              }
             ]
     },
     {   // installation instructions
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,13 +1,177 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  return license;
+// reference array to get data for user-selected license
+const licenseData = [
+  // any change to this array needs to be reflected in the one used by index.js
+  {
+    license: 'Apache License 2.0',
+    badge: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    link: 'https://opensource.org/licenses/Apache-2.0',
+  },
+  {
+    license: 'Boost Software License 1.0',
+    badge: '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)',
+    link: 'https://www.boost.org/LICENSE_1_0.txt',
+  },
+  {
+    license: 'BSD 3-Clause License',
+    badge: '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+    link: 'https://opensource.org/licenses/BSD-3-Clause',
+  },
+  {
+    license: 'BSD 2-Clause License',
+    badge: '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)',
+    link: 'https://opensource.org/licenses/BSD-2-Clause',
+  },
+  {
+    license: 'Creative Commons CC0 1.0',
+    badge: '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)',
+    link: 'http://creativecommons.org/publicdomain/zero/1.0/',
+  },
+  {
+    license: 'Creative Commons Attribution 4.0 International',
+    badge: '[![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)',
+    link: 'https://creativecommons.org/licenses/by/4.0/',
+  },
+  {
+    license: 'Creative Commons Attribution-ShareAlike 4.0 International',
+    badge: '[![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC_BY--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)',
+    link: 'https://creativecommons.org/licenses/by-sa/4.0/',
+  },
+  {
+    license: 'Creative Commons Attribution-NonCommercial 4.0 International',
+    badge: '[![License: CC BY-NC 4.0](https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc/4.0/)',
+    link: 'https://creativecommons.org/licenses/by-nc/4.0/',
+  },
+  {
+    license: 'Creative Commons Attribution-NoDerivates 4.0 International',
+    badge: '[![License: CC BY-ND 4.0](https://img.shields.io/badge/License-CC_BY--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nd/4.0/)',
+    link: 'https://creativecommons.org/licenses/by-nd/4.0/',
+  },
+  {
+    license: 'Creative Commons Attribution-NonCommmercial-ShareAlike 4.0 International',
+    badge: '[![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)',
+    link: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+  },
+  {
+    license: 'Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International',
+    badge: '[![License: CC BY-NC-ND 4.0](https://img.shields.io/badge/License-CC_BY--NC--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-nd/4.0/)',
+    link: 'https://creativecommons.org/licenses/by-nc-nd/4.0/',
+  },
+  {
+    license: 'Eclipse Public License 1.0',
+    badge: '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)',
+    link: 'https://opensource.org/licenses/EPL-1.0',
+  },
+  {
+    license: 'GNU GPL v3',
+    badge: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+    link: 'https://www.gnu.org/licenses/gpl-3.0',
+  },
+  {
+    license: 'GNU GPL v2',
+    badge: '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)',
+    link: 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html',
+  },
+  {
+    license: 'GNU AGPL v3',
+    badge: '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)',
+    link: 'https://www.gnu.org/licenses/agpl-3.0',
+  },
+  {
+    license: 'GNU LGPL v3',
+    badge: '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)',
+    link: 'https://www.gnu.org/licenses/lgpl-3.0',
+  },
+  {
+    license: 'GNU FDL v1.3',
+    badge: '[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)',
+    link: 'https://www.gnu.org/licenses/fdl-1.3',
+  },
+  {
+    license: 'The Hippocratic License 2.1',
+    badge: '[![License: Hippocratic 2.1](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg)](https://firstdonoharm.dev)',
+    link: 'https://firstdonoharm.dev',
+  },
+  {
+    license: 'The Hippocratic License 3.0',
+    badge: '[![License: Hippocratic 3.0](https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg)](https://firstdonoharm.dev)',
+    link: 'https://firstdonoharm.dev',
+  },
+  {
+    license: 'IBM Public License Version 1.0',
+    badge: '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)',
+    link: 'https://opensource.org/licenses/IPL-1.0',
+  },
+  {
+    license: 'Open Source Initiative ISC License',
+    badge: '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
+    link: 'https://opensource.org/licenses/ISC',
+  },
+  {
+    license: 'The MIT License',
+    badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    link: 'https://opensource.org/licenses/MIT',
+  },
+  {
+    license: 'Mozilla Public License 2.0',
+    badge: '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)',
+    link: 'https://opensource.org/licenses/MPL-2.0',
+  },
+  {
+    license: 'Open Data Commons Attribution License (BY)',
+    badge: '[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)',
+    link: 'https://opendatacommons.org/licenses/by/',
+  },
+  {
+    license: 'Open Database License (ODbL)',
+    badge: '[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)',
+    link: 'https://opendatacommons.org/licenses/odbl/',
+  },
+  {
+    license: 'Public Domain Dedication and License (PDDL)',
+    badge: '[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)',
+    link: 'https://opendatacommons.org/licenses/pddl/',
+  },
+  {
+    license: 'Perl Artistic License 2.0',
+    badge: '[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)',
+    link: 'https://opensource.org/licenses/Artistic-2.0',
+  },
+  {
+    license: 'SIL Open Font License 1.1',
+    badge: '[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)',
+    link: 'https://opensource.org/licenses/OFL-1.1',
+  },
+  {
+    license: 'The Unlicense',
+    badge: '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)',
+    link: 'http://unlicense.org/',
+  },
+  {
+    license: 'The Do What the F*ck You Want to Public License',
+    badge: '[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)',
+    link: 'http://www.wtfpl.net/about/',
+  },
+  {
+    license: 'The zlib/libpng License',
+    badge: '[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)',
+    link: 'https://opensource.org/licenses/Zlib',
+  }
+]
+
+// return a license badge based on the user-selected license
+function renderLicenseBadge(licenseIndex) {
+  if (licenseIndex) {
+    return `
+${licenseData[licenseIndex].badge}
+`;
+  } else {
+    return '';
+  };
 };
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  return license;
+// return the license link
+function renderLicenseLink(licenseIndex) {
+  return licenseData[licenseIndex].link;
 };
 
 // generate the license section of README
@@ -18,7 +182,7 @@ function renderLicenseSection(license) {
 
 ## License
     
-${renderLicenseLink(license)}`
+This project is licensed under [${licenseData[data.licenseIndex].license}](${renderLicenseLink(data.licenseIndex)}).`
   } else {
     // if there is no license, return an empty string
     return "";
@@ -26,8 +190,8 @@ ${renderLicenseLink(license)}`
 };
 
 // if there is a license, add it to the table of contents
-function renderLicenseTOC(license) {
-  if (license) {
+function renderLicenseTOC(licenseIndex) {
+  if (licenseIndex) {
     return `
 - [License](#license)`;
   } else {
@@ -38,9 +202,7 @@ function renderLicenseTOC(license) {
 // generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  
-${renderLicenseBadge(data.license)}
-
+${renderLicenseBadge(data.licenseIndex)}
 ## Description
 
 ${data.description}
@@ -48,7 +210,7 @@ ${data.description}
 ## Table of Contents
 
 - [Installation](#installation)
-- [Usage](#usage)${renderLicenseTOC(data.license)}
+- [Usage](#usage)${renderLicenseTOC(data.licenseIndex)}
 - [Contributing](#contributing)
 - [Tests](#tests)
 - [Questions](#questions)
@@ -59,7 +221,7 @@ ${data.installation}
 
 ## Usage
 
-${data.usage}${renderLicenseSection(data.license)}
+${data.usage}${renderLicenseSection(data.licenseIndex)}
 
 ## Contributing
 
